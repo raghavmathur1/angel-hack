02fe1b1689ac1aa70dc8097dea8df885e0735427
Raghav Mathur <raghav3501@gmail.com>
2018-05-06 08:57:40 +0530

    python 1

--- angel-hack.py
+++ angel-hack.py
@@ -10,6 +10,7 @@
   def initialize():
       """Download required nltk libraries."""
       textrank.setup_environment()
+
   @cli.command()
   @click.argument('filename')
   def extract_summary(filename):
@@ -24,16 +25,16 @@
       with open(filename) as f:
           phrases = textrank.extract_key_phrases(f.read())
           print(phrases)
+
   @cli.command()
   @click.argument('text')
   def get_suma(text):
       suma = textrank.extract_sentences(text)
-  get_suma()
+ 
+
  app = Flask(_name_)
  @app.route("/")
+
  def hello():
- txt = request.args.get('txt')
-get_suma(txt)
-   print "lmao"
-     print "nubz"
-print("nubv")
+ 	txt = request.args.get('txt')
+	get_suma(txt)
